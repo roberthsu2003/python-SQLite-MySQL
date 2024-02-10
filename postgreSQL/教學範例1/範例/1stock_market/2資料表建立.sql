ba501eaaaf0d631feb7bc2f41398a12d62ff2c79
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-10 13:17:30 +0800

    modify

--- postgreSQL/教學範例1/範例/1stock_market/2資料表建立.sql
+++ postgreSQL/教學範例1/範例/1stock_market/2資料表建立.sql
@@ -3,15 +3,22 @@ CREATE TABLE IF NOT EXISTS 市場(
 	country VARCHAR(20),
 	PRIMARY KEY(name)
 );
+/*
+Syntax: NUMERIC(precision, scale)
+
+Where,
+     Precision: Total number of digits.
+     Scale: Number of digits in terms of a fraction.
+*/
 
 CREATE TABLE IF NOT EXISTS 股市(
 	stock_id SERIAL,
 	date DATE,
-	open NUMERIC(7,6),
-	high NUMERIC(7,6),
-	low NUMERIC(7,6),
-	close NUMERIC(7,6),
-	adj_close NUMERIC(7,6),
+	open NUMERIC(17,10),
+	high NUMERIC(17,10),
+	low NUMERIC(17,10),
+	close NUMERIC(17,10),
+	adj_close NUMERIC(17,10),
 	volume BIGINT DEFAULT 0,
 	name VARCHAR(20),
 	PRIMARY KEY(stock_id),
